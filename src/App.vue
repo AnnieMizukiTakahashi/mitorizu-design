9d8c158d4217ba1824c6e974dc25065e79c84dfe
MizukiTakahashi <mizukitakahashi.design@gmail.com>
2020-03-16 04:07:14 +0900

    footer, headerをコンポーネント化して作成

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,32 @@
 <template>
   <div id="app">
-      <header class="hero is-fullheight">
-          <nav class="navbar">
-              <div class="navbar-brand">
-                  <span class="navbar-burger burger has-text-white" data-target="navbarMenu" @click="toggleMenu" :class="{'is-active': isMenuActive}">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </span>
-              </div>
-              <div id="navbarMenu" class="navbar-menu" :class="{'is-active': isMenuActive}">
-                  <div class="navbar-end">
-                      <div class="tabs mr-56 is-one-quarter-desktop is-right">
-                        <ul>
-                          <li>
-                            <router-link class="has-text-white" to="/">Top</router-link>
-                          </li>
-                          <li>
-                            <router-link class="has-text-white" to="about">About</router-link>
-                          </li>
-                          <li>
-                            <router-link class="has-text-white" to="learn">Learn</router-link>
-                          </li>
-                          <li>
-                            <router-link class="has-text-white" to="theme">Theme</router-link>
-                          </li>
-                          <li>
-                            <router-link class="has-text-white" to="academic">AcademicFild</router-link>
-                          </li>
-                        </ul>
-                      </div>
-                  </div>
-              </div>
-          </nav>
-      </header>
-    <router-view/>
+    <header class="hero">
+        <Header/>
+    </header>
+      <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Header from './components/header.vue'
+import Footer from './components/footer.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
 <style lang="scss">
-@import "src/styles/_variables.scss";
+@import "styles/_variables.scss";
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP|Sawarabi+Gothic&display=swap');
 
 #app {
-  font-family:Helvetica, Arial, sans-serif;
+  font-family:'Sawarabi Gothic', 'Noto Sans JP', Helvetica, Arial, sans-serif;
 }
-.navbar-brand{
-  background-color: $main-color;
-}
-.navbar-menu {
-  background-color: $main-color;
-  margin: auto;
-}
-.tabs{
-  font-size: 20px;
-  font-weight: bold;
-}
+
 </style>
---
+++ src/components/theme.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container is-desktop hero is-fullheight">
+        <p>Theme</p>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+@import '../styles/_variables.scss';
+</style>
