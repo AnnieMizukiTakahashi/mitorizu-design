242f8d137845d12f20eda3cade956a79affd45d7
MizukiTakahashi <mizukitakahashi.design@gmail.com>
2020-04-12 17:14:17 +0900

    add buefy

--- src/components/molecules/MainVisualCard.vue
+++ src/components/molecules/MainVisualCard.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="container">
-    <p class="subtitle is-size-6">ヨーロッパにおける宗教と美術には
-どんな関わりがあったのか</p>
-    <p class="card-title is-size-5 has-text-weigt-bold ">Vol.2 絵画はキリスト教をいかに
-広く伝搬しえたのか</p>
-
+    <p class="subtitle is-size-6">
+      ヨーロッパにおける宗教と美術にはどんな関わりがあったのか</p>
+    <p class="card-title is-size-5 has-text-weigt-bold ">
+      Vol.2 絵画はキリスト教をいかに広く伝搬しえたのか</p>
   </div>
 </template>
